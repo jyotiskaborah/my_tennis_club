d11696ef1b8a55fa78581e4cd7ab85556c671c0d
Jyotiska Borah <jyotiskaborah@gmail.com>
2024-09-22 18:40:01 +0530

    si

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(*q%1bz$9trlz4h484$isq7x23zk1q2zx$^+9#9k-*48mj=uz$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['ec2-3-110-113-105.ap-south-1.compute.amazonaws.com']
 
 
 # Application definition
