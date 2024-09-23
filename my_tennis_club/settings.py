ece95ae4ec9a1190ec6e95bfcdc2985c2e62f9a2
Jyotiska Borah <jyotiskaborah@gmail.com>
2024-09-23 19:23:13 +0530

    removed .extentions floder

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(*q%1bz$9trlz4h484$isq7x23zk1q2zx$^+9#9k-*48mj=uz$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.110.113.105']
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
